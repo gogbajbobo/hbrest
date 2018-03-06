8816b7590d7c6911a468c9954fc67a245e252fb6
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-06 16:53:26 +0300

    app = express()

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const mysql = require('mysql');
 const debug = require('debug')('hbrest:app');
 
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.listen(port, () => {
+    debug('HomeBudget RESTful API server started on: ' + port);
+});
+
 const con = mysql.createConnection({
     host: "localhost",
     user: "hbclient",
