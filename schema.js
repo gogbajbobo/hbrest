39f5970b15395a9001d49a0c957f46213c5adea7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-07 14:14:01 +0300

    schema.js

---
+++ schema.js
@@ -0,0 +1,65 @@
+const _ = require('lodash');
+
+const foreignKey = {
+    type: 'integer',
+    nullable: false,
+    unsigned: true
+};
+
+const Account = {
+    name: {
+        type: 'string',
+        maxlength: 254,
+        nullable: false,
+        unique: true
+    },
+    value: {
+        type: 'decimal',
+        nullable: false,
+        unsigned: false
+    },
+    currency: {
+        type: 'string',
+        maxlength: 10,
+        nullable: false
+    }
+};
+
+const IncomeAccount = Account;
+const ActiveAccount = Account;
+const ExpenseAccount = Account;
+
+const SubAccount = _.assign(Account, {
+    mainAccount_id: foreignKey
+});
+
+const Transaction = {
+    date: {
+        type: 'dateTime',
+        nullable: false
+    },
+    fromValue: {
+        type: 'decimal',
+        nullable: false,
+        unsigned: false
+    },
+    toValue: {
+        type: 'decimal',
+        nullable: false,
+        unsigned: false
+    },
+    fromAccount_id: foreignKey,
+    toAccount_id: foreignKey
+};
+
+const Schema = {
+    incomeAccounts: IncomeAccount,
+    activeAccounts: ActiveAccount,
+    expenseAccounts: ExpenseAccount,
+    subAccounts: SubAccount,
+    transactions: Transaction
+};
+
+console.log(Schema);
+
+module.exports = Schema;
