45ae38fc4c96401e373fd8cd4d72177c6893b73c
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-08 15:21:01 +0300

    implement get Account types with params

--- routes/accountTypes.js
+++ routes/accountTypes.js
@@ -4,6 +4,7 @@ const
     Bookshelf = require('bookshelf')(knex),
     router = require('express').Router(),
     uuidv4 = require('uuid/v4'),
+    _ = require('lodash'),
     routesHelper = require('./helpers/routesHelper');
 
 const {AccountType, AccountTypes} = require('../models/AccountType.model');
@@ -23,21 +24,17 @@ function setupRoutes() {
 
         .get((req, res) => {
 
-            debug('get all account types');
-            debug(req.query);
-
-            AccountTypes.forge()
-                .fetch()
-                .then(collection => {
-                    routesHelper.responseWithObject(res, collection);
-                })
-                .catch(err => {
-                    routesHelper.responseWithError(res, err);
-                });
+            if (_.keys(req.query).length > 0) {
+                responseWithParams(res, req.query);
+            } else {
+                responseWithAllObjects(res);
+            }
 
         })
 
         .post((req, res) => {
+
+            debug('create account type with params', req.body);
 
             AccountType.forge({
                 type: req.body.type,
@@ -81,6 +78,8 @@ function setupRoutes() {
 
         .put((req, res) => {
 
+            debug('update account type #', req.params.id, 'with params', req.body);
+
             AccountType.forge({id: req.params.id})
                 .fetch({require: true})
                 .then(accountType => {
@@ -104,6 +103,8 @@ function setupRoutes() {
 
         .delete((req, res) => {
 
+            debug('delete account type #', req.params.id);
+
             AccountType.forge({id: req.params.id})
                 .fetch({require: true})
                 .then(accountType => {
@@ -121,6 +122,46 @@ function setupRoutes() {
                     routesHelper.responseWithError(res, err);
                 });
 
+        });
+
+}
+
+function responseWithAllObjects(res) {
+
+    debug('get all account types');
+
+    AccountTypes.forge()
+        .fetch()
+        .then(collection => {
+            routesHelper.responseWithObject(res, collection);
+        })
+        .catch(err => {
+            routesHelper.responseWithError(res, err);
+        });
+
+}
+
+function responseWithParams(res, params) {
+
+    debug('get account types with params', params);
+
+    AccountTypes.forge()
+        .query('where', params)
+        .fetch()
+        .then(accountType => {
+
+            if (!accountType) {
+
+                const err = new Error('accountType with params ' + params + ' not found');
+                routesHelper.responseWithError(res, err, 404);
+
+            } else {
+                routesHelper.responseWithObject(res, accountType);
+            }
+
+        })
+        .catch(err =>  {
+            routesHelper.responseWithError(res, err);
         });
 
 }
