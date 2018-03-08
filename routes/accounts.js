66ecf59a4c082a7539bc4107c2ab312890d253de
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-08 16:29:28 +0300

    add routes for account

--- routes/accounts.js
+++ routes/accounts.js
@@ -4,6 +4,7 @@ const
     Bookshelf = require('bookshelf')(knex),
     router = require('express').Router(),
     uuidv4 = require('uuid/v4'),
+    _ = require('lodash'),
     routesHelper = require('./helpers/routesHelper');
 
 const {Account, Accounts} = require('../models/Account.model');
@@ -23,18 +24,17 @@ function setupRoutes() {
 
         .get((req, res) => {
 
-            Accounts.forge()
-                .fetch()
-                .then(collection => {
-                    routesHelper.responseWithObject(res, collection);
-                })
-                .catch(err => {
-                    routesHelper.responseWithError(res, err);
-                });
+            if (_.keys(req.query).length > 0) {
+                responseWithParams(res, req.query);
+            } else {
+                responseWithAllObjects(res);
+            }
 
         })
 
         .post((req, res) => {
+
+            debug('create account with params', req.body);
 
             Account.forge({
                 name: req.body.name,
@@ -51,6 +51,124 @@ function setupRoutes() {
                     routesHelper.responseWithError(res, err);
                 });
 
+        });
+
+    router.route('/accounts/:id')
+
+        .get((req, res) => {
+
+            debug('get account #', req.params.id);
+
+            Account.forge({id: req.params.id})
+                .fetch()
+                .then(account => {
+
+                    if (!account) {
+
+                        const err = new Error('account #' + req.params.id + ' not found');
+                        routesHelper.responseWithError(res, err, 404);
+
+                    } else {
+                        routesHelper.responseWithObject(res, account);
+                    }
+
+                })
+                .catch(err =>  {
+                    routesHelper.responseWithError(res, err);
+                });
+
+        })
+
+        .put((req, res) => {
+
+            debug('update account #', req.params.id, 'with params', req.body);
+
+            Account.forge({id: req.params.id})
+                .fetch({require: true})
+                .then(account => {
+
+                    account.save({
+                        name: req.body.name || account.get('name'),
+                        value: req.body.value || account.get('value'),
+                        currency: req.body.currency || account.get('currency'),
+                        xid: req.body.xid || account.get('xid'),
+                        accountType_id: req.body.accountType_id || account.get('accountType_id')
+                    })
+                        .then(() => {
+                            routesHelper.responseWithObject(res, account);
+                        })
+                        .catch(err => {
+                            routesHelper.responseWithError(res, err);
+                        });
+
+                })
+                .catch(err => {
+                    routesHelper.responseWithError(res, err);
+                });
+
+        })
+
+        .delete((req, res) => {
+
+            debug('delete account #', req.params.id);
+
+            Account.forge({id: req.params.id})
+                .fetch({require: true})
+                .then(account => {
+
+                    account.destroy()
+                        .then(() => {
+                            routesHelper.responseWithObject(res, account);
+                        })
+                        .catch(err => {
+                            routesHelper.responseWithError(res, err);
+                        });
+
+                })
+                .catch(err => {
+                    routesHelper.responseWithError(res, err);
+                });
+
+        });
+
+}
+
+function responseWithAllObjects(res) {
+
+    debug('get all accounts');
+
+    Accounts.forge()
+        .fetch()
+        .then(collection => {
+            routesHelper.responseWithObject(res, collection);
+        })
+        .catch(err => {
+            routesHelper.responseWithError(res, err);
+        });
+
+}
+
+function responseWithParams(res, params) {
+
+    debug('get accounts with params', params);
+
+    Accounts.forge()
+        .query('where', params)
+        .fetch()
+        .then(accounts => {
+
+            if (!accounts) {
+
+                const err = new Error('accountType with params ' + params + ' not found');
+                routesHelper.responseWithError(res, err, 404);
+
+            } else {
+                routesHelper.responseWithObject(res, accounts);
+            }
+
+        })
+        .catch(err =>  {
+            routesHelper.responseWithError(res, err);
         });
 
 }
