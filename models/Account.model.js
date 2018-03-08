0a845b0b1cb593ad5d0d442ef5d279e53012627b
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-08 17:29:15 +0300

    add static properties/funcs to model/collection

--- models/Account.model.js
+++ models/Account.model.js
@@ -1,6 +1,7 @@
 const
     knex = require('../db/knex'),
-    Bookshelf = require('bookshelf')(knex);
+    Bookshelf = require('bookshelf')(knex),
+    uuidv4 = require('uuid/v4');
 
 const {AccountType} = require('./AccountType.model');
 const {Transaction} = require('./Transaction.model');
@@ -28,10 +29,34 @@ const Account = Bookshelf.Model.extend({
         return this.hasMany(Transaction, 'toAccount_id');
     }
 
+}, {
+
+    creationProperties: params => {
+        return {
+            name: params.name,
+            value: params.value,
+            currency: params.currency,
+            xid: params.xid || uuidv4(),
+            accountType_id: params.accountType_id
+        };
+    },
+    updatingProperties: (params, object) => {
+        return {
+            name: params.name || object.get('name'),
+            value: params.value || object.get('value'),
+            currency: params.currency || object.get('currency'),
+            xid: params.xid || object.get('xid'),
+            accountType_id: params.accountType_id || object.get('accountType_id')
+        };
+    },
+    logName: 'account'
+
 });
 
 const Accounts = Bookshelf.Collection.extend({
     model: Account
+}, {
+    logName: 'accounts'
 });
 
 module.exports = {Account, Accounts};
