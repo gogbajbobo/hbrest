deec388999023e0d4c8d582a9b1f2523ae08e907
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-09 23:40:44 +0300

    add routes/dataRoutes.js

---
+++ routes/dataRoutes.js
@@ -0,0 +1,54 @@
+const
+    router = require('express').Router(),
+    _ = require('lodash'),
+    routesHelper = require('./helpers/routesHelper'),
+    path = require('path');
+
+const models = [
+    require('../models/Account.model'),
+    require('../models/AccountType.model'),
+    require('../models/Transaction.model')
+];
+
+module.exports = () => {
+
+    _.forEach(models, model => {
+        setupRoutes(model.objectModel, model.collectionModel)
+    });
+    return router;
+
+};
+
+function setupRoutes(objectModel, collectionModel) {
+
+    router.route(path.join('/', collectionModel.path))
+
+        .get((req, res) => {
+
+            if (_.keys(req.query).length > 0) {
+                routesHelper.responseWithObjectWithParams(res, req.query, collectionModel);
+            } else {
+                routesHelper.responseWithAllObjects(res, collectionModel);
+            }
+
+        })
+
+        .post((req, res) => {
+            routesHelper.createObjectWithParams(res, req.body, objectModel);
+        });
+
+    router.route(path.join('/', collectionModel.path, ':id'))
+
+        .get((req, res) => {
+            routesHelper.getObjectWithId(res, req.params.id, objectModel);
+        })
+
+        .put((req, res) => {
+            routesHelper.updateObjectWithParams(res, req.params.id, req.body, objectModel);
+        })
+
+        .delete((req, res) => {
+            routesHelper.deleteObjectWithId(res, req.params.id, objectModel);
+        });
+
+}
