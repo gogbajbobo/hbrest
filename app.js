307c44168815efe59603443abc3b60b91d8d103d
gogbajbobo <grigoriev.maxim@gmail.com>
2018-03-09 16:02:44 +0300

    refactor a bit

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const
     debug = require('debug')('hbrest:app'),
     express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    Schema = require('./db/schema'),
+    _ = require('lodash');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const
-    apiBaseUrl = '/api',
-    accountTypeRoutes = require('./routes/accountTypes'),
-    accountRoutes = require('./routes/accounts'),
-    transactionRoutes = require('./routes/transactions');
+const apiBaseUrl = '/api';
 
-app.use(apiBaseUrl, accountTypeRoutes());
-app.use(apiBaseUrl, accountRoutes());
-app.use(apiBaseUrl, transactionRoutes());
+// routes files in ./routes should be named exactly as tables in Schema
+
+_.forEach(_.keys(Schema), key => {
+    app.use(apiBaseUrl, require('./routes/' + key)());
+});
 
 const port = process.env.PORT || 8888;
 
